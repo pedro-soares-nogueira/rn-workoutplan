ee22c40bc59b07b0d04a129a48c7e5b03679d3c8
Pedro Soares <dev.pedro.nogueira@gmail.com>
2023-03-22 17:58:47 -0300

    feat:: header component

---
+++ src/components/HomeHeader.tsx
@@ -0,0 +1,28 @@
+import { Heading, HStack, Icon, Text, VStack } from "native-base"
+import { TouchableOpacity } from "react-native"
+import { UserPhoto } from "./UserPhoto"
+import { MaterialIcons } from "@expo/vector-icons"
+
+export function HomeHeader() {
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={6} alignItems="center">
+      <UserPhoto
+        source={{ uri: "https://github.com/pedro-soares-nogueira.png" }}
+        size={16}
+        alt="Imagem do usuário"
+        mr={6}
+      />
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Olá,
+        </Text>
+
+        <Heading color="gray.100">Pedro</Heading>
+      </VStack>
+
+      <TouchableOpacity>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  )
+}
