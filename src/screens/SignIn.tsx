2c316ebc7ca03f852230cfe9d56a6eea247ef5f9
Pedro Soares <dev.pedro.nogueira@gmail.com>
2023-03-22 15:54:40 -0300

    styling:: button component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,6 +2,7 @@ import { VStack, Image, Center, Text, Heading } from "native-base"
 
 import BackgroundImg from "../assets/background.png"
 import LogoSvg from "../assets/logo.svg"
+import { Button } from "../components/Button"
 import { Input } from "../components/Input"
 
 export function SignIn() {
@@ -34,6 +35,8 @@ export function SignIn() {
           autoCapitalize="none"
         />
         <Input placeholder="Senha" secureTextEntry />
+
+        <Button title="Acessar" />
       </Center>
     </VStack>
   )
