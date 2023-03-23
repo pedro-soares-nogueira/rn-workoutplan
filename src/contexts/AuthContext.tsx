784cfa21fe3774586d30b0b4cc4c219a6c61e4ae
Pedro Soares <dev.pedro.nogueira@gmail.com>
2023-03-23 14:46:53 -0300

    taks:: get storage config

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,6 +1,7 @@
-import { createContext, ReactNode, useState } from "react"
+import { createContext, ReactNode, useEffect, useState } from "react"
 import { UserDTO } from "../dtos/UserDTO"
 import { api } from "../services/api"
+import { storageUserGet, storageUserSave } from "../storage/storageUser"
 
 export type AuthContextDataProps = {
   user: UserDTO
@@ -24,11 +25,24 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
       if (data.user) {
         setUser(data.user)
+        storageUserSave(data.user)
       }
     } catch (error) {
       throw error
     }
   }
+
+  const loadUserData = async () => {
+    const userLogged = await storageUserGet()
+
+    if (userLogged) {
+      setUser(userLogged)
+    }
+  }
+
+  useEffect(() => {
+    loadUserData()
+  }, [])
 
   return (
     <AuthContext.Provider
